b6709a7f3bafd87d3483a4ff566490f1bf36f66b
hehe <jeradrhinehart@gmail.com>
2025-05-20 02:56:58 -0700

    change gui to use customtkinter gui is now dark theme and cleaner

--- gui.py
+++ gui.py
@@ -12,74 +12,76 @@ from media.state import MediaState
 from exporter import export_image, export_video
 
 # Tkinter imports
+import customtkinter as ctk
+from tkinter import filedialog, messagebox
 import tkinter as tk
-from tkinter import filedialog, ttk, messagebox
 
 
 # Create window
-window = tk.Tk()
+ctk.set_appearance_mode("dark")
+ctk.set_default_color_theme("blue")
+window = ctk.CTk()
 window.title("Ditherer")
 window.geometry("600x900")
 window.minsize(600, 900)
-style = ttk.Style()
-
+theme_color = "#242424"
 
 ## Frames and labels
 
-# Load image button frame
-load_button_frame = ttk.Frame(window)
+# Load media button frame
+load_button_frame = ctk.CTkFrame(window, fg_color=theme_color)
 load_button_frame.place(relx=0.5, rely=0.05, anchor="center")
 
 # Grayscale checkbox frame
-grayscale_frame = ttk.Frame(window)
+grayscale_frame = ctk.CTkFrame(window, fg_color=theme_color)
 grayscale_frame.place(relx=0.5, rely=0.9, relwidth=0.4, relheight=0.1, anchor="center")
 
 # Algo dropdown frame
-dropdown_frame = ttk.Frame(window)
+dropdown_frame = ctk.CTkFrame(window, fg_color=theme_color)
 dropdown_frame.place(relx=0.5, rely=0.56, relwidth=0.4, relheight=0.05, anchor="center")
 
 # Algo submenu frame
-floyd_steinberg_submenu = ttk.Frame(window)
+floyd_steinberg_submenu = ctk.CTkFrame(window, fg_color=theme_color)
 
-submenu_dropdown_frame = ttk.Frame(window)
+submenu_dropdown_frame = ctk.CTkFrame(window, fg_color=theme_color)
 
 
 # Export buttons frame
-export_frame = ttk.Frame(window)
+export_frame = ctk.CTkFrame(window, fg_color=theme_color)
 export_frame.place(relx=0.5, rely=0.92, relwidth=0.6, anchor="center")
 
 # Image frame
-image_frame = ttk.Frame(window)
+image_frame = ctk.CTkFrame(window)
 image_frame.place(relx=0.5, rely=0.3, relwidth=0.8, relheight=0.4, anchor="center")
 image_frame.pack_propagate(False)
 
 # Slider frame
-downscale_factor = tk.IntVar(value=2)
-slider_frame = ttk.Frame(window)
+downscale_factor = ctk.IntVar(value=2)
+slider_frame = ctk.CTkFrame(window, fg_color=theme_color)
 slider_frame.place(relx=0.5, rely=0.80, relwidth=0.7, relheight=0.1, anchor="center")
 
 # Label to display image
-image_label = tk.Label(image_frame, bg="#d0d0d0")
-image_label.pack(fill=tk.BOTH, expand=True)
+image_label = ctk.CTkLabel(image_frame, text="")
+image_label.pack(fill=ctk.BOTH, expand=True)
 
 # Label to display Slider
-slider_label = ttk.Label(
+slider_label = ctk.CTkLabel(
     slider_frame, text="Downscale Factor:", anchor="w", justify="left"
 )
 slider_label.pack(anchor="w")
 
 # Algorithm dropdown label frame
-algo_label_frame = ttk.Frame(window)
+algo_label_frame = ctk.CTkFrame(window, fg_color=theme_color)
 algo_label_frame.place(
     relx=0.5, rely=0.515, relwidth=0.3, relheight=0.03, anchor="center"
 )
 
 # Algorithm dropdown label
-algo_label = ttk.Label(algo_label_frame, text="Dithering Algorithm", anchor="w")
+algo_label = ctk.CTkLabel(algo_label_frame, text="Dithering Algorithm", anchor="w")
 algo_label.pack()
 
 # Algorithm submenu label frame
-algo_submenulabel_frame = ttk.Frame(window)
+algo_submenulabel_frame = ctk.CTkFrame(window, fg_color=theme_color)
 algo_submenulabel_frame.place(
     relx=0.5, rely=0.59, relwidth=0.3, relheight=0.03, anchor="center"
 )
@@ -134,7 +136,7 @@ def update_image():
         resized_image = resize_to_fit(loaded_image, frame_width, frame_height)
 
         image_tk = ImageTk.PhotoImage(resized_image)
-        image_label.config(image=image_tk)
+        image_label.configure(image=image_tk)
         image_label.image = image_tk
 
 
@@ -153,44 +155,43 @@ def on_resize(event):
 # Bind the resize of window to image update
 window.bind("<Configure>", on_resize)
 
-###########
-# Widgets #
-###########
+# Widgets 
 # Load button
-load_media_button = ttk.Button(load_button_frame, text="Load Media", command=load_media)
+load_media_button = ctk.CTkButton(load_button_frame, text="Load Media", command=load_media)
 load_media_button.pack(pady=10)
 
 # Grayscale checkbox
-grayscale_var = tk.BooleanVar()
-grayscale_checkbox = ttk.Checkbutton(
+grayscale_var = ctk.BooleanVar()
+grayscale_checkbox = ctk.CTkCheckBox(
     grayscale_frame, text="Convert to grayscale?", variable=grayscale_var
 )
 grayscale_checkbox.pack()
 
-# Algorithm dropdown
-options_list = ("Bayer", "Floyd-Steinberg")
-selected_option = tk.StringVar(value=options_list[0])
-dropdown = ttk.Combobox(dropdown_frame, textvariable=selected_option)
-dropdown["values"] = options_list
-dropdown["state"] = "readonly"
-dropdown.pack()
-
-dropdown_submenulabel = ttk.Label(algo_submenulabel_frame, anchor="w")
-
 # Algo submenus
 # Bayer submenu
-submenu_options_list = ("2x2", "4x4", "8x8", "16x16")
-submenu_selected_option = tk.StringVar(value=submenu_options_list[0])
-dropdown_submenu = ttk.Combobox(
-    submenu_dropdown_frame, textvariable=submenu_selected_option
+submenu_options_list = ["2x2", "4x4", "8x8", "16x16"]
+dropdown_submenu = ctk.CTkComboBox(
+    master=submenu_dropdown_frame,
+    values=submenu_options_list,
+    state="readonly"
 )
-dropdown_submenu["values"] = submenu_options_list
-dropdown_submenu["state"] = "readonly"
+dropdown_submenu.set(submenu_options_list[0])
+dropdown_submenu.pack()
 # Floyd-Steinberg submenu
 sliders = []
 for i in range(4):
     initial_value = [7, 3, 5, 1]
-    slide = tk.Scale(floyd_steinberg_submenu, orient="horizontal", from_=-24, to=24)
+    slide = tk.Scale(
+        floyd_steinberg_submenu,
+        orient="horizontal",
+        from_=-24,
+        to=24,
+        bg=theme_color,
+        troughcolor="#282828",
+        highlightthickness=0,
+        highlightbackground=theme_color,
+        fg="#FFFFFF"
+)
     slide.set(initial_value[i])
     sliders.append(slide)
 sliders[0].grid(row=0, column=0)  # Top left
@@ -198,9 +199,10 @@ sliders[1].grid(row=0, column=1)  # Top right
 sliders[2].grid(row=1, column=0)  # Bottom left
 sliders[3].grid(row=1, column=1)  # Bottom right
 
-
-def show_submenu(*args):
-    if dropdown.get() == "Bayer":
+# Algorithm dropdown
+dropdown_submenulabel = ctk.CTkLabel(algo_submenulabel_frame, anchor="w")
+def show_submenu(selected_value):
+    if selected_value == "Bayer":
         floyd_steinberg_submenu.place_forget()
         submenu_dropdown_frame.place(
             relx=0.5, rely=0.63, relwidth=0.95, relheight=0.05, anchor="center"
@@ -209,35 +211,50 @@ def show_submenu(*args):
         dropdown_submenu.pack()
         dropdown_submenulabel.pack()
 
-    elif dropdown.get() == "Floyd-Steinberg":
+    elif selected_value == "Floyd-Steinberg":
         submenu_dropdown_frame.place_forget()
         floyd_steinberg_submenu.place(
-            relx=0.5, rely=0.68, relwidth=0.523, relheight=0.15, anchor="center"
+            relx=0.5, rely=0.65, relheight=0.09, anchor="center"
         )
         dropdown_submenulabel.configure(text="Weights")
 
-
-dropdown.bind("<<ComboboxSelected>>", show_submenu)
-show_submenu()
+options_list = ["Bayer", "Floyd-Steinberg"]
+dropdown = ctk.CTkComboBox(
+    master=dropdown_frame,
+    values=options_list,
+    command=show_submenu,
+    state="readonly"
+)
+dropdown.set(options_list[0])
+dropdown.pack()
+show_submenu(dropdown.get())
 
 # Downscale Slider
 downscale_slider = tk.Scale(
-    slider_frame, from_=1, to=12, orient=tk.HORIZONTAL, variable=downscale_factor
+    slider_frame,
+    orient="horizontal",
+    from_=1, to=12,
+    variable=downscale_factor,
+    bg=theme_color,
+    troughcolor="#282828",
+    highlightthickness=0,
+    highlightbackground=theme_color,
+    fg="#FFFFFF"
 )
-downscale_slider.pack(fill=tk.X, expand=True)
+downscale_slider.pack(fill=ctk.X, expand=True)
 
 # Progress bar
-progress_var = tk.DoubleVar()
-progress_bar = ttk.Progressbar(window, variable=progress_var, maximum=100)
+progress_var = ctk.DoubleVar()
+progress_bar = ctk.CTkProgressBar(window, variable=progress_var)
 progress_bar.place(relx=0.5, rely=0.97, relwidth=0.9, anchor="center")
 
 # Export buttons
-export_png_button = ttk.Button(
+export_png_button = ctk.CTkButton(
     export_frame, text="Export PNG", command=lambda: export_image(
-        window,
-        dropdown,
-        dropdown_submenu,
-        loaded_image,
+        window=window,
+        dropdown=dropdown,
+        dropdown_submenu=dropdown_submenu,
+        loaded_image=loaded_image,
         grayscale_enabled=grayscale_var.get(),
         downscale=downscale_factor.get(),
         format="png",
@@ -247,11 +264,13 @@ export_png_button = ttk.Button(
         fs_dither=fs_dither,
         progress_callback=progress_var.set
 ))
-export_jpg_button = ttk.Button(
+
+export_jpg_button = ctk.CTkButton(
     export_frame, text="Export JPG", command=lambda: export_image(
-        dropdown,
-        dropdown_submenu,
-        loaded_image,
+        window=window,
+        dropdown=dropdown,
+        dropdown_submenu=dropdown_submenu,
+        loaded_image=loaded_image,
         grayscale_enabled=grayscale_var.get(),
         downscale=downscale_factor.get(),
         format="jpeg",
@@ -259,10 +278,10 @@ export_jpg_button = ttk.Button(
         apply_grayscale=apply_grayscale,
         apply_bayer_dithering=apply_bayer_dithering,
         fs_dither=fs_dither,
-        progress_callback=progress_var.set,
-        window=window
+        progress_callback=progress_var.set
 ))
-export_video_button = ttk.Button(
+
+export_video_button = ctk.CTkButton(
     export_frame, text="Export Video", command=lambda: export_video(
         window,
         dropdown,
@@ -280,17 +299,15 @@ export_video_button = ttk.Button(
     )
 )
 
-
 def export_buttons(*args):
     if media_state.is_video:
         export_jpg_button.pack_forget()
         export_png_button.pack_forget()
-        export_video_button.pack(side=tk.TOP, expand=True)
+        export_video_button.pack(side=ctk.TOP, expand=True)
     else:
         export_video_button.pack_forget()
-        export_png_button.pack(side=tk.RIGHT, expand=True)
-        export_jpg_button.pack(side=tk.LEFT, expand=True)
-
+        export_png_button.pack(side=ctk.RIGHT, expand=True)
+        export_jpg_button.pack(side=ctk.LEFT, expand=True)
 
 # Tinker Event loop
 window.mainloop()
